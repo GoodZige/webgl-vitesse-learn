bb7d4c0e3a1a0506295ec4b153db50b9a5c788cb
zhengzijue <zhengzijue@wanji.net.cn>
2023-07-14 15:43:52 +0800

    first commit

---
+++ src/api/ConvexCameraHelper.js
@@ -0,0 +1,154 @@
+/* eslint-disable no-console */
+// import { Camera } from '../cameras/Camera.js';
+// import { Vector3 } from '../math/Vector3.js';
+// import { LineSegments } from '../objects/LineSegments.js';
+// import { Color } from '../math/Color.js';
+// import { LineBasicMaterial } from '../materials/LineBasicMaterial.js';
+// import { BufferGeometry } from '../core/BufferGeometry.js';
+// import { Float32BufferAttribute } from '../core/BufferAttribute.js';
+import * as THREE from 'three'
+import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry.js'
+
+const _vector = /* @__PURE__ */ new THREE.Vector3()
+
+class ConvexCameraHelper extends THREE.Mesh {
+  constructor(matrixWorld, projectionMatrixInverse) {
+    const geometry = new THREE.BufferGeometry()
+    const material = new THREE.MeshBasicMaterial({ color: 0x00FF00 })
+
+    const vertices = []
+    const colors = []
+
+    const pointMap = {}
+
+    addPoint('n1')
+    addPoint('n2')
+    addPoint('n3')
+    addPoint('n4')
+
+    addPoint('p')
+
+    function addPoint(id) {
+      vertices.push(0, 0, 0)
+      colors.push(0, 0, 0)
+
+      if (pointMap[id] === undefined)
+
+        pointMap[id] = []
+
+      pointMap[id].push((vertices.length / 3) - 1)
+    }
+
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
+
+    super(geometry, material)
+
+    this.type = 'ConvexCameraHelper'
+
+    this.matrix = matrixWorld// 需要相机的世界矩阵
+    this.matrixAutoUpdate = false
+
+    this.pointMap = pointMap
+
+    this.projectionMatrixInverse = projectionMatrixInverse
+
+    this.update()
+    // colors
+    this.drawConvexCameraHelper(geometry.getAttribute('position').array)
+  }
+
+  // n1 n2 n3 n4 p
+  drawConvexCameraHelper(positionArray) {
+    const pointV3Array = []
+    positionArray.forEach((e, i) => {
+      if ((i + 1) % 3 === 0) {
+        pointV3Array.push(
+          new THREE.Vector3(
+            positionArray[i - 2],
+            positionArray[i - 1],
+            positionArray[i],
+          ),
+        )
+      }
+    })
+
+    const [n1, n2, n3, n4, p] = pointV3Array
+    const r = p.distanceTo(n1)
+
+    const [p_n1, p_n2, p_n3, p_n4] = pointV3Array
+      .slice(0, 4)
+      .map(e => e.sub(p)) // 向量
+
+    const angle_pn1_pn3 = p_n1.angleTo(p_n3)
+    const SEGEMENT = 10
+    const angle_segement = angle_pn1_pn3 / SEGEMENT
+
+    const arcGroup = []
+    const drawArcPoints = (v1, v2) => {
+      const points = []
+      const nextV = v1.clone()
+      const normalV = new THREE.Vector3().crossVectors(v1, v2).normalize()
+      for (let i = 0; i < SEGEMENT; i++) {
+        // nextV.applyAxisAngle(normalV, -0.1).normalize().multiplyScalar(r) ; //得到旋转后的向量
+        nextV.applyAxisAngle(normalV, angle_segement) // 得到旋转后的向量
+        const track_p = new THREE.Vector3().addVectors(nextV, p) // 得到旋转后在弧面上的点
+        arcGroup.push(track_p)
+        points.push(track_p)
+      }
+      return points
+    }
+
+    const pointArray_12 = drawArcPoints(p_n1, p_n2)
+    const pointArray_13 = drawArcPoints(p_n1, p_n3)
+    const pointArray_24 = drawArcPoints(p_n2, p_n4)
+    const pointArray_34 = drawArcPoints(p_n3, p_n4)
+
+    console.log(pointArray_12, pointArray_34)
+    for (let i = 0; i < pointArray_12.length; i++)
+      drawArcPoints(pointArray_12[i], pointArray_34[i])
+
+    const drawPoints = [...arcGroup, ...pointV3Array]
+    this.geometry = new ConvexGeometry(drawPoints)
+  }
+
+  update() {
+    const geometry = this.geometry
+    const pointMap = this.pointMap
+
+    const w = 0.7
+    const h = 1
+
+    // we need just camera projection matrix inverse
+    // world matrix must be identity
+
+    // near
+
+    setPoint('n1', pointMap, geometry, this.matrixWorld, this.projectionMatrixInverse, -w, -h, 1)
+    setPoint('n2', pointMap, geometry, this.matrixWorld, this.projectionMatrixInverse, w, -h, 1)
+    setPoint('n3', pointMap, geometry, this.matrixWorld, this.projectionMatrixInverse, -w, h, 1)
+    setPoint('n4', pointMap, geometry, this.matrixWorld, this.projectionMatrixInverse, w, h, 1)
+
+    geometry.getAttribute('position').needsUpdate = true
+  }
+
+  dispose() {
+    this.geometry.dispose()
+    this.material.dispose()
+  }
+}
+
+function setPoint(point, pointMap, geometry, matrixWorld, projectionMatrixInverse, x, y, z) {
+  // _vector.set( x, y, z ).unproject( camera );
+  _vector.set(x, y, z).applyMatrix4(projectionMatrixInverse).applyMatrix4(matrixWorld)
+  const points = pointMap[point]
+
+  if (points !== undefined) {
+    const position = geometry.getAttribute('position')
+
+    for (let i = 0, l = points.length; i < l; i++)
+
+      position.setXYZ(points[i], _vector.x * 0.002, _vector.y * 0.002, _vector.z * 0.002)
+  }
+}
+
+export { ConvexCameraHelper }
---
+++ src/layouts/home.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'HomeLayout',
+})
+
+const isCollapse = ref(true)
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<template>
+  <main px-4 py-10 text="center gray-700 dark:gray-200">
+    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+      <el-radio-button :label="false">
+        expand
+      </el-radio-button>
+      <el-radio-button :label="true">
+        collapse
+      </el-radio-button>
+    </el-radio-group>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      :collapse="isCollapse"
+      @open="handleOpen"
+      @close="handleClose"
+    >
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon><location /></el-icon>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group>
+          <template #title>
+            <span>Group One</span>
+          </template>
+          <el-menu-item index="1-1">
+            item one
+          </el-menu-item>
+          <el-menu-item index="1-2">
+            item two
+          </el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group Two">
+          <el-menu-item index="1-3">
+            item three
+          </el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="1-4">
+          <template #title>
+            <span>item four</span>
+          </template>
+          <el-menu-item index="1-4-1">
+            item one
+          </el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <el-icon><icon-menu /></el-icon>
+        <template #title>
+          Navigator Two
+        </template>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <el-icon><document /></el-icon>
+        <template #title>
+          Navigator Three
+        </template>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon><setting /></el-icon>
+        <template #title>
+          Navigator Four
+        </template>
+      </el-menu-item>
+    </el-menu>
+    <RouterView />
+    <TheFooter />
+    <div mx-auto mt-5 text-center text-sm opacity-50>
+      [Home Layout]
+    </div>
+  </main>
+</template>
